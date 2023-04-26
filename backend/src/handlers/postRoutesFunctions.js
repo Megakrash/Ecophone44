dc3d9e5452f8b8a76d17663302e98e589143f09c
Jonathan Scattolini <jscattolini@gmail.com>
2023-04-26 18:47:01 +0200

    :hammer: Add createModel

--- backend/src/handlers/postRoutesFunctions.js
+++ backend/src/handlers/postRoutesFunctions.js
@@ -15,6 +15,26 @@ const postNewBrand = (req, res) => {
     });
 };
 
+// POST NEW MODEL
+const postNewModel = (req, res) => {
+  const { name, filename, marqueId } = req.body;
+
+  database
+    .query("INSERT INTO modele(name, pic, marque_id) VALUES (?, ?, ?);", [
+      name,
+      filename,
+      marqueId,
+    ])
+    .then(() => {
+      res.status(201).send({ message: "Model Added" });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error add new Model");
+    });
+};
+
 module.exports = {
   postNewBrand,
+  postNewModel,
 };
--- backend/src/routes/multers/multers.js
+++ backend/src/routes/multers/multers.js
@@ -1,19 +1,40 @@
 const multer = require("multer");
+const path = require("path");
 // const fs = require("fs");
 
-// Import des videos dans le backend
+// Import & storage brand pic
 const storageBrandPic = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public/assets/images/marques");
   },
   filename: (req, file, cb) => {
-    req.body.filename = `${req.body.name.toUpperCase()}.jpg`;
+    const ext = path.extname(file.originalname);
+    req.body.filename = `${req.body.name.toUpperCase()}${
+      ext === ".jpg" ? ".jpg" : ".png"
+    }`;
     cb(null, req.body.filename.toString());
   },
 });
 
 const uploadBrandPic = multer({ storage: storageBrandPic });
 
+// Import & storage model pic
+const storageModelPic = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/assets/images/modeles");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    req.body.filename = `${req.body.name.toUpperCase()}${
+      ext === ".jpg" ? ".jpg" : ".png"
+    }`.replace(/\s+/g, "");
+    cb(null, req.body.filename.toString());
+  },
+});
+
+const uploadModelPic = multer({ storage: storageModelPic });
+
 module.exports = {
   uploadBrandPic,
+  uploadModelPic,
 };
