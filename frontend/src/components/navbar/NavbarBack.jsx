2cd3982f2815b55051b4b0a26ead52d8ddc8d36f
Jonathan Scattolini <jscattolini@gmail.com>
2023-06-06 23:14:34 +0200

    :hammer: Update Navbar back office

--- frontend/src/components/navbar/NavbarBack.jsx
+++ frontend/src/components/navbar/NavbarBack.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function NavbarBack() {
+function NavbarBack({
+  setChoosenBrandId,
+  setChoosenModelId,
+  setShowCreateBrand,
+  setShowType,
+}) {
   return (
     <div className="navbarBack">
       <div className="navbarBack_logo">
-        <img
-          className="navbarBack_logo_img"
-          src={`${
-            import.meta.env.VITE_PORT_BACKEND
-          }/assets/images/general/logo-simple.png`}
-          alt="Ecophone 44"
-        />
+        <button
+          className="navbarBack_logo_btn"
+          type="button"
+          onClick={() => {
+            setChoosenBrandId(0);
+            setChoosenModelId(0);
+            setShowCreateBrand(false);
+            setShowType(null);
+          }}
+        >
+          <img
+            className="navbarBack_logo_btn_img"
+            src={`${
+              import.meta.env.VITE_PORT_BACKEND
+            }/assets/images/general/logo-simple.png`}
+            alt="Ecophone 44"
+          />
+        </button>
       </div>
       <div className="navbarBack_title">
         <p>BACK-OFFICE</p>
@@ -20,3 +37,10 @@ function NavbarBack() {
 }
 
 export default NavbarBack;
+
+NavbarBack.propTypes = {
+  setChoosenBrandId: PropTypes.func.isRequired,
+  setChoosenModelId: PropTypes.func.isRequired,
+  setShowCreateBrand: PropTypes.func.isRequired,
+  setShowType: PropTypes.func.isRequired,
+};
