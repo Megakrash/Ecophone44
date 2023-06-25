9251cd338f3f869763a4f54faef8375383b7580e
Jonathan Scattolini <jscattolini@gmail.com>
2023-06-25 11:11:42 +0200

    :sparkles: Add ScrollToTop component :hammer: vite.config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "@components/scrollToTop/ScrollToTop";
 import App from "./App";
 import "./components/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  // <React.StrictMode>
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
-  // </React.StrictMode>
 );
