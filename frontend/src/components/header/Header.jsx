6e86bab76d75ce2e052d2cc0c1d816e38d350860
Jonathan Scattolini <jscattolini@gmail.com>
2023-07-27 15:23:48 +0200

    :sparkles: Add Carousel

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,57 @@
 import React from "react";
+import C from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 4000, min: 700 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
+
+const sliderData = [
+  {
+    id: 1,
+    pic: "pub1.png",
+  },
+  {
+    id: 2,
+    pic: "pub2.png",
+  },
+  {
+    id: 3,
+    pic: "pub3.png",
+  },
+  {
+    id: 4,
+    pic: "pub4.png",
+  },
+];
 
 function Header() {
+  const Carousel = C.default ? C.default : C;
+  const picPath = `${import.meta.env.VITE_PORT_BACKEND}/assets/images/general/`;
   return (
     <div className="header">
-      {/* <div className="header_text">
-        <p>Votre réparateur de Smartphones & Tablettes</p>
-        <p>sur Nantes depuis 2012</p>
-      </div> */}
-      <img
-        className="header_img"
-        src={`${
-          import.meta.env.VITE_PORT_BACKEND
-        }/assets/images/general/header.jpg`}
-        alt="Ecophone 44"
-      />
+      <Carousel
+        containerClass=""
+        responsive={responsive}
+        infinite
+        autoPlay
+        autoPlaySpeed={5000}
+      >
+        {sliderData.map(({ id, pic }) => {
+          return (
+            <img
+              key={id}
+              className="header_img"
+              src={picPath + pic}
+              alt="Ecophone 44"
+            />
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
